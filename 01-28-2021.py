34dda16d85e9b4f691c595dbc10e94cecd36d172
YAEEUNKANG <76837991+YaeeunKANG@users.noreply.github.com>
2021-01-28 23:56:59 +0900

    01-28 Daily Coding

---
+++ 01-28-2021.py
@@ -0,0 +1,30 @@
+'''01-28-2021 Baekjoon Algorithm
+단계별 문제 풀이 - 9단계
+언어 - Python'''
+
+
+
+# 1085
+# 한수는 지금 (x, y)에 있다. 직사각형의 왼쪽 아래 꼭짓점은 (0, 0)에 있고, 오른쪽 위 꼭짓점은 (w, h)에 있다. 
+# 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
+x, y, w, h = map(int, input().split())
+print(min([x, y, w-x, h-y]))
+
+
+
+# 3009
+# 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+
+x_ = []
+y_ = []
+for i in range(3):
+        x, y = map(int, input().split())
+        x_.append(x)
+        y_.append(y)
+for i in range(3):
+        if x_.count(x_[i]) == 1:
+                x = x_[i]
+        if y_.count(y_[i]) == 1:
+                y = y_[i]
+print(x, y)
